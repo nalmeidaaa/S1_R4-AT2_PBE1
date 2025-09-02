9a3abd55d2707b137426c5d2bb0993a02baaa4da
unknown <nabpessoal@gmail.com>
2025-09-02 13:17:04 -0300

    first commit

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-
 const express = require('express');
 const app = express();
 const PORT = 8081;
 const fs = require("fs")
 
-app.get("/livros", (req, res) => { //GET -> Coleta os dados da "/livros"
+app.get("/produtos/:paginas", (req, res) => { //GET -> Coleta os dados da "/produtos"
     try {
         // Ler o arquivo JSON
-        const data = fs.readFileSync('./livros.json', 'utf-8');
-        const { titulo, autor, ano } = req.query //Foi usado "dataEvento", pois se eu colocar "data", daria conflito com a constante de cima
+        const data = fs.readFileSync('./produtos.json', 'utf-8');
+        const { paginas } = req.params //Foi usado "dataEvento", pois se eu colocar "data", daria conflito com a constante de cima
 
         // Conversão de JSON em Objeto JS
-        let livros = JSON.parse(data); //Variavel de ambiente do node, para transformar um objeto JSON em JavaScript
+        let produtos = JSON.parse(data); //Variavel de ambiente do node, para transformar um objeto JSON em JavaScript
 
-        //Título do livro
-        if ( titulo ) {
-            livros = livros.filter(
-                livro => livro.titulo.toLowerCase().includes(titulo.toLowerCase())); // Devolve um vetor com os elementos que a condição passar nessa condição
+        let pagina; //A função slice (ou slicing) serve para extrair uma porção (uma "fatia") de uma sequência de dados, como um array ou uma string, e retornar essa porção como uma nova sequência, sem modificar a original
+
+        if (paginas == 1) {
+            pagina = produtos.slice(0, 10);
+        } else if (paginas == 2) {
+            pagina = produtos.slice(10, 20);
+        } else if (paginas == 3) {
+            pagina = produtos.slice(20, 30);
+        } else {
+            return res.status(404).json({ message: "Página não encontrada" });
         }
 
-        //Autor do livro
-        if ( autor ) {
-            livros = livros.filter(
-                livro => livro.autor.toLowerCase().includes(autor.toLowerCase()));
-        }
-
-        //Ano do livro
-        if ( ano ) {
-            livros = livros.filter(
-                livro => livro.ano == ano)
-        }
-
-        res.status(200).json(livros); // Resposta com o código de status 200 (que significa "Sucesso Geral"), e a anexar a um corpo da resposta o objeto ou array produtos no formato JSON
+        res.status(200).json(pagina); // Resposta com o código de status 200 (que significa "Sucesso Geral"), e a anexar a um corpo da resposta o objeto ou array produtos no formato JSON
 
     } catch (error) {
         console.error("Erro interno ao ler o arquivo Json", error);
