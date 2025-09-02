cd43a8873084a97e2ff24eb0dd9fab4c44d7822c
unknown <nabpessoal@gmail.com>
2025-09-02 10:46:03 -0300

    first commit

--- app.js
+++ app.js
@@ -4,22 +4,34 @@ const app = express();
 const PORT = 8081;
 const fs = require("fs")
 
-app.get("/usuarios", (req, res) => { //GET -> Coleta os dados da "/usuarios"
+app.get("/livros", (req, res) => { //GET -> Coleta os dados da "/livros"
     try {
         // Ler o arquivo JSON
-        const data = fs.readFileSync('./usuarios.json', 'utf-8');
-        const { nome } = req.query
+        const data = fs.readFileSync('./livros.json', 'utf-8');
+        const { titulo, autor, ano } = req.query //Foi usado "dataEvento", pois se eu colocar "data", daria conflito com a constante de cima
 
         // Conversão de JSON em Objeto JS
-        let usuarios = JSON.parse(data); //Variavel de ambiente do node, para transformar um objeto JSON em JavaScript
+        let livros = JSON.parse(data); //Variavel de ambiente do node, para transformar um objeto JSON em JavaScript
 
-        if ( nome ) {
-            usuarios = usuarios.filter(
-                usuario => usuario.nome.toLowerCase().includes(nome.toLowerCase())); // Devolve um vetor com os elementos que a condição passar nessa condição
-
+        //Título do livro
+        if ( titulo ) {
+            livros = livros.filter(
+                livro => livro.titulo.toLowerCase().includes(titulo.toLowerCase())); // Devolve um vetor com os elementos que a condição passar nessa condição
         }
 
-        res.status(200).json(usuarios); // Resposta com o código de status 200 (que significa "Sucesso Geral"), e a anexar a um corpo da resposta o objeto ou array produtos no formato JSON
+        //Autor do livro
+        if ( autor ) {
+            livros = livros.filter(
+                livro => livro.autor.toLowerCase().includes(autor.toLowerCase()));
+        }
+
+        //Ano do livro
+        if ( ano ) {
+            livros = livros.filter(
+                livro => livro.ano == ano)
+        }
+
+        res.status(200).json(livros); // Resposta com o código de status 200 (que significa "Sucesso Geral"), e a anexar a um corpo da resposta o objeto ou array produtos no formato JSON
 
     } catch (error) {
         console.error("Erro interno ao ler o arquivo Json", error);
