e5eb17c1f7989a25122a78c3f489b3d1835339ca
unknown <nabpessoal@gmail.com>
2025-09-02 10:36:55 -0300

    second commit

--- app.js
+++ app.js
@@ -4,22 +4,21 @@ const app = express();
 const PORT = 8081;
 const fs = require("fs")
 
-app.get("/usuarios", (req, res) => { //GET -> Coleta os dados da "/usuarios"
+app.get("/eventos", (req, res) => { //GET -> Coleta os dados da "/eventos"
     try {
         // Ler o arquivo JSON
-        const data = fs.readFileSync('./usuarios.json', 'utf-8');
-        const { nome } = req.query
+        const data = fs.readFileSync('./eventos.json', 'utf-8');
+        const { dataEvento } = req.query //Foi usado "dataEvento", pois se eu colocar "data", daria conflito com a constante de cima.
 
         // Conversão de JSON em Objeto JS
-        let usuarios = JSON.parse(data); //Variavel de ambiente do node, para transformar um objeto JSON em JavaScript
+        let eventos = JSON.parse(data); //Variavel de ambiente do node, para transformar um objeto JSON em JavaScript
 
-        if ( nome ) {
-            usuarios = usuarios.filter(
-                usuario => usuario.nome.toLowerCase().includes(nome.toLowerCase())); // Devolve um vetor com os elementos que a condição passar nessa condição
-
+        if ( dataEvento ) {
+            eventos = eventos.filter(
+                evento => evento.data.includes(dataEvento)); // Devolve um vetor com os elementos que a condição passar nessa condição
         }
 
-        res.status(200).json(usuarios); // Resposta com o código de status 200 (que significa "Sucesso Geral"), e a anexar a um corpo da resposta o objeto ou array produtos no formato JSON
+        res.status(200).json(eventos); // Resposta com o código de status 200 (que significa "Sucesso Geral"), e a anexar a um corpo da resposta o objeto ou array produtos no formato JSON
 
     } catch (error) {
         console.error("Erro interno ao ler o arquivo Json", error);
